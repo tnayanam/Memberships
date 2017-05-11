b7ac64c8326dce42b1638669b660cfce17ebc18c
tnayanam <nayatanuj@gmail.com>
2017-05-10 21:36:01 -0400

    Added read only property in model

--- Memberships/Entities/Item.cs
+++ Memberships/Entities/Item.cs
@@ -1,4 +1,9 @@
-﻿namespace Memberships.Entities
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
+
+namespace Memberships.Entities
 {
     [Table("Item")]
     public class Item
@@ -8,6 +13,33 @@
         [Required]
         [MaxLength(255)]
         public string Title { get; set; }
+
+        [MaxLength(2048)]
+        public string Description { get; set; }
+
+        [MaxLength(1024)]
+        public string Url { get; set; }
+
+        [MaxLength(1024)]
+        public string ImageUrl { get; set; }
+
+        [AllowHtml]
+        public string HTML { get; set; }
+
+        [DefaultValue(0)]
+        public int WaitDays { get; set; }
+
+        public string HTMLShort
+        {
+            get
+            {
+                return HTML == null ||
+                    HTML.Length < 50 ? HTML : HTML.Substring(0, 50);
+            }
+        }
+
+
+
     }
 
 }
