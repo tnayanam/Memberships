17fcfa573cab779897989b90ea7ec973929f843d
tnayanam <nayatanuj@gmail.com>
2017-05-13 19:29:54 -0400

    Added dropdown using Extension Methods

---
+++ Memberships/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Memberships.Extensions
+{
+    public static class ReflectionExtensions
+    {
+        public static string GetPropertyValue<T>(this T item, string propertyName)
+        {
+            return item.GetType()
+                .GetProperty(propertyName)
+                .GetValue(item, null)
+                .ToString();
+        }
+
+
+    }
+}
